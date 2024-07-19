e6fd047730f182ae5e5c4fd22d6aa436e48cdbb0
Pavin Wannasirilak <w.matt.pavin@gmail.com>
2024-07-20 04:39:40 +0700

    add some request's methods for testing

--- httputils/httpt/request.go
+++ httputils/httpt/request.go
@@ -12,16 +12,32 @@ func NewRequestPost(target string, body io.Reader) *http.Request {
 	return httptest.NewRequest(http.MethodPost, target, body)
 }
 
-func NewRequestPostJson(target string, v any) (*http.Request, error) {
-	body := new(bytes.Buffer)
-	if err := json.NewEncoder(body).Encode(v); err != nil {
-		return nil, err
-	}
-	r := NewRequestPost(target, body)
-	r.Header.Set("Content-Type", "application/json")
-	return r, nil
+func NewRequestPut(target string, body io.Reader) *http.Request {
+	return httptest.NewRequest(http.MethodPut, target, body)
+}
+
+func NewRequestDelete(target string) *http.Request {
+	return httptest.NewRequest(http.MethodDelete, target, nil)
 }
 
 func NewRequestGet(target string) *http.Request {
 	return httptest.NewRequest(http.MethodGet, target, nil)
+}
+
+func NewRequestPostJson(target string, v any) (*http.Request, error) {
+	return jsonRequest(target, v, NewRequestPost)
+}
+
+func NewRequestPutJson(target string, v any) (*http.Request, error) {
+	return jsonRequest(target, v, NewRequestPut)
+}
+
+func jsonRequest(target string, v any, createRequest func(string, io.Reader) *http.Request) (*http.Request, error) {
+	body := new(bytes.Buffer)
+	if err := json.NewEncoder(body).Encode(v); err != nil {
+		return nil, err
+	}
+	r := createRequest(target, body)
+	r.Header.Set("Content-Type", "application/json")
+	return r, nil
 }
