93f7f5d4cacb9105515dba07c77c75f64a64b9cb
Pavin Wannasirilak <w.matt.pavin@gmail.com>
2024-08-08 22:02:53 +0700

    change dlayq -> jsondq package name

---
+++ jsondq/queue.go
@@ -0,0 +1,68 @@
+package jsondq
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/hdt3213/delayqueue"
+	"github.com/matthxwpavin/ticketing/logging/sugar"
+	"github.com/redis/go-redis/v9"
+)
+
+// Queue is a delayed queue assumes that messages/payload production and
+// consumption in the queue must be encoded and decoded using json.Marshal/json.Unmarshal.
+// When unmarshaling has an error occurred then the queue itself do a good acknowledge(return true)
+// to do not want to consume the message/payload again by re-delivery mechanic.
+type Queue[T any] struct {
+	q *delayqueue.DelayQueue
+}
+
+func New[T any](
+	ctx context.Context,
+	name string,
+	c *redis.Client,
+	process func(*T) bool,
+) *Queue[T] {
+	return &Queue[T]{
+		q: applyOptions(delayqueue.NewQueue(name, c, func(payloadStr string) bool {
+			payload := new(T)
+			if err := json.Unmarshal([]byte(payloadStr), payload); err != nil {
+				sugar.FromContext(ctx).
+					With("queue", name).
+					Errorw("could not unmarshal payload", "error", err)
+				// Mark the message as acknowleded to don't want to consume again
+				// from re-delivery mechanic, due to the message is invalid JSON.
+				return true
+			}
+			return process(payload)
+		})),
+	}
+}
+
+func (q *Queue[T]) SendDelayMsg(payload *T, d time.Duration) error {
+	data, err := marshalAsString(payload)
+	if err != nil {
+		return err
+	}
+	return q.q.SendDelayMsg(data, d)
+}
+
+func (q *Queue[T]) SendScheduleMsg(payload *T, t time.Time) error {
+	data, err := marshalAsString(payload)
+	if err != nil {
+		return err
+	}
+	return q.q.SendScheduleMsg(data, t)
+}
+
+func applyOptions(q *delayqueue.DelayQueue) *delayqueue.DelayQueue {
+	return q.WithConcurrent(4).
+		WithDefaultRetryCount(3).
+		WithMaxConsumeDuration(6 * time.Minute)
+}
+
+func marshalAsString(a any) (string, error) {
+	data, err := json.Marshal(a)
+	return string(data), err
+}
