4b63b8a8078e7e59ccd09b55f663073b3ef1cbf8
Pavin Wannasirilak <w.matt.pavin@gmail.com>
2024-08-11 00:17:37 +0700

    update order created message fields

--- streaming/order_created.go
+++ streaming/order_created.go
@@ -15,9 +15,11 @@ type OrderCreatedConsumer interface {
 type OrderCreatedMessage struct {
 	OrderId   string    `json:"orderId"`
 	Status    string    `json:"orderStatus"`
+	Version   int32     `json:"version"`
 	ExpiresAt time.Time `json:"expiresAt"`
 	Ticket    struct {
 		Id    string  `json:"id"`
 		Price float64 `json:"price"`
 	} `json:"ticket"`
+	UserId string `json:"user_id"`
 }
